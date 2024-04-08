9c441c707d25ad641f1983cb4056e6dcd9f0410b
Matthew Lai <84052183+mklai10@users.noreply.github.com>
2024-04-08 16:37:51 -0700

    Add files via upload

---
+++ fighters.pl
@@ -0,0 +1,87 @@
+fighter(mario, male, [human], mario, 98, [ssb_64], [arcade], 1981).
+fighter(donkey_kong, male, [kong], donkey_kong, 127, [ssb_64], [arcade], 1981).
+fighter(link, male, [human], the_legend_of_zelda, 104, [ssb_64], [nes], 1986).
+fighter(samus, female, [augmented, human], metroid, 108, [ssb_64], [nes], 1986).
+fighter(dark_samus, female, [augmented, human], metroid, 108, [ssb_ultimate], [gamecube], 2002).
+fighter(yoshi, male, [yoshi], mario, 104, [ssb_64], [snes], 1990).
+fighter(kirby, male, [kirby], kirby, 79, [ssb_64], [gameboy], 1992).
+fighter(fox, male, [fox], star_fox, 77, [ssb_64], [snes], 1993).
+fighter(pikachu, other, [pokemon], pokemon, 79, [ssb_64], [gameboy], 1996).
+fighter(luigi, male, [human], mario, 97, [ssb_64], [arcade], 1983).
+fighter(ness, male, [human], earthbound, 94, [ssb_64], [snes], 1994).
+fighter(captain_falcon, male, [human], f-zero, 104, [ssb_64], [snes], 1990).
+fighter(jigglypuff, other, [pokemon], pokemon, 68, [ssb_64], [gameboy], 1996).
+fighter(peach, female, [human], mario, 89, [ssb_mario], [nes], 1985).
+fighter(daisy, female, [human], mario, 89, [ssb_ultimate], [gameboy], 1989).
+fighter(bowser, male, [koopa], mario, 135, [ssb_mario], [nes], 1985).
+fighter(ice_climbers, other, [human], ice_climbers, 92, [ssb_mario], [arcade], 1984).
+fighter(sheik, female, [human], the_legend_of_zelda, 78, [ssb_mario], [nintendo_64], 1998).
+fighter(zelda, female, [human], the_legend_of_zelda, 85, [ssb_mario], [nes], 1986).
+fighter(dr_mario, male, [human], mario, 98, [ssb_mario], [gameboy,nes], 1990).
+fighter(pichu, other, [pokemon], pokemon, 62, [ssb_mario], [gameboy_color], 1999).
+fighter(falco, male, [bird], star_fox, 82, [ssb_mario], [snes], 1993).
+fighter(marth, male, [human], fire_emblem, 90, [ssb_melee], [famicom], 1990).
+fighter(lucina, female, [augmented,human], fire_emblem, 90, [ssb_3ds, wii_u], [nintendo_3ds], 2012).
+fighter(young_link, male, [human], the_legend_of_zelda, 88, [ssb_melee], [nintendo_64], 1998).
+fighter(ganondorf, male, [demon,human], the_legend_of_zelda, 118, [ssb_melee], [nintendo_64], 1998).
+fighter(mewtwo, other, [pokemon], pokemon, 79, [ssb_melee], [gameboy], 1996).
+fighter(roy, male, [human], fire_emblem, 95, [ssb_melee], [gba], 2001).
+fighter(chrom, male, [augmented,human], fire_emblem, 95, [ssb_ultimate], [nintendo_3ds], 2012).
+fighter(mr_game_and_watch, male, [monochrome], game_and_watch, 75, [ssb_melee], [game_and_watch], 1980).
+fighter(meta_knight, male, [unknown], kirby, 80, [ssb_brawl], [nes], 1993).
+fighter(pit, male, [angel], kid_icarus, 96, [ssb_brawl], [nes], 1986).
+fighter(dark_pit, male, [angel], kid_icarus, 96, [ssb_3ds, wii_u], [nintendo_3ds], 2012).
+fighter(zero_suit_samus, female, [augmented,human], metroid, 80, [ssb_brawl], [gba], 1986).
+fighter(wario, male, [human], mario, 107, [ssb_brawl], [gameboy], 1992).
+fighter(snake, male, [augmented,human], metal_gear_solid, 106, [ssb_brawl], [msx2], 1987).
+fighter(ike, male, [human], fire_emblem, 107, [ssb_brawl], [gamecube], 2005).
+fighter(pokemon_trainer, selectable, [human,pokemon], pokemon, 75, [ssb_brawl], [gameboy], 1996).
+fighter(diddy_kong, male, [kong], donkey_kong, 90, [ssb_brawl], [snes], 1994).
+fighter(lucas, male, [human], earthbound, 94, [ssb_brawl], [gba], 2006).
+fighter(sonic, male, [hedgehog], sonic_the_hedgehog, 86, [ssb_brawl], [sega_genesis], 1990).
+fighter(king_dedede, male, [bird], kirby, 127, [ssb_brawl], [gameboy], 1992).
+fighter(olimar, male, [hocotatian], pikmin, 79, [ssb_brawl], [gamecube], 2001).
+fighter(lucario, other, [pokemon], pokemon, 92, [ssb_brawl], [nintendo_ds], 2005).
+fighter(rob, male, [robot], rob, 106, [ssb_brawl], [nes], 1985).                                              % idk how to do r.o.b 
+fighter(toon_link, male, [human], the_legend_of_zelda, 91, [ssb_brawl], [gamecube], 2002).
+fighter(wolf, male, [wolf], star_fox, 92, [ssb_brawl], [nintendo_64], 1997).
+fighter(villager, selectable, [human], animal_crossing, 92, [ssb_3ds, wii_u], [nintendo_64], 2001).
+fighter(mega_man, male, [robot], mega_man, 102, [ssb_3ds, wii_u], [nes], 1987).
+fighter(wii_fit_trainer, selectable, [human], wii_fit, 96, [ssb_3ds, wii_u], [wii], 2007).
+fighter(rosalina_and_luma, female, [human, luma], mario, 82, [ssb_3ds,wii_u], [wii], 2007).
+fighter(little_mac, male, [human], punch_out, 87, [ssb_3ds,wii_u], [nes], 2007).
+fighter(greninja, other, [pokemon], pokemon, 88, [ssb_3ds,wii_u], [nintendo_3ds], 2013).
+fighter(mii_brawler, selectable, [human], ssb, 94, [ssb_3ds,wii_u], [wii], 2006).
+fighter(mii_swordfighter, selectable, [human], ssb, 100, [ssb_3ds,wii_u], [wii], 2006).
+fighter(mii_gunner, selectable, [human], ssb, 104, [ssb_3ds,wii_u], [wii], 2006).
+fighter(palutena, female, [god], kid_icarus, 91, [ssb_3ds,wii_u], [nes], 1986).
+fighter(pac_man, male, [pac-person], pac-man, 95, [ssb_3ds,wii_u], [arcade], 1980).
+fighter(robin, selectable, [augmented,human], fire_emblem, 95, [ssb_3ds,wii_u], [nintendo_3ds], 2012).
+fighter(shulk, male, [homs], xenoblade, 97, [ssb_3ds,wii_u], [wii], 2010).
+fighter(bowser_jr, selectable, [koopa], mario, 108, [ssb_3ds,wii_u], [gamecube], 2002).
+fighter(duck_hunt, other, [bird,dog], duck_hunt, 86, [ssb_3ds,wii_u], [nes], 1984).
+fighter(ryu, male, [human], street_fighter, 103, [ssb_3ds,wii_u], [arcade], 1987).
+fighter(ken, male, [human], street_fighter, 103, [ssb_3ds,wii_u], [arcade], 1987).
+fighter(cloud, male, [augmented,human], final_fantasy, 100, [ssb_3ds,wii_u], [playstation], 1997).
+fighter(corrin, selectable, [dragon,human], fire_emblem, 98, [ssb_3ds,wii_u], [nintendo_3ds], 2015).
+fighter(bayonetta, female, [augmented,human], bayonetta, 81, [ssb_3ds,wii_u], [playstation_3,xbox_360], 2009).
+fighter(inkling, selectable, [inkling], splatoon, 94, [ssb_ultimate], [wii_u], 2015).
+fighter(ridley, male, [unknown], metroid, 107, [ssb_ultimate], [nes], 1986).
+fighter(simon, male, [human], castlevania, 107, [ssb_ultimate], [nes], 1986).
+fighter(richter, male, [human], castlevania, 107, [ssb_ultimate], [turbografx-16], 1993).
+fighter(king_k_rool, male, [crocodile], donkey_kong, 133, [ssb_ultimate], [snes], 1994).
+fighter(isabelle, female, [dog], animal_crossing, 88, [ssb_ultimate], [nintendo_3ds], 2012).
+fighter(incineroar, other, [pokemon], pokemon, 116, [ssb_ultimate], [nintendo_3ds], 2016).
+fighter(piranha_plant, other, [plant], mario, 112, [ssb_ultimate], [nes], 1985).
+fighter(joker, male, [human], persona, 93, [ssb_ultimate], [playstation_3,playstation_4], 2016).
+fighter(hero, male, [human,hybrid], dragon_quest, 101, [ssb_ultimate], [nes], 1986).
+fighter(banjo_and_kazooie, other, [bear,bird], banjo-kazooie, 106, [ssb_ultimate], [nintendo_64], 1997).
+fighter(terry, male, [human], the_king_of_fighters, 108, [ssb_ultimate], [arcade], 1991).
+fighter(byleth, selectable, [god,human], fire_emblem, 97, [ssb_ultimate], [android,ios,nintendo_switch], 2019).
+fighter(min_min, female, [augmented,human], arms, 104, [ssb_ultimate], [nintendo_switch], 2017).
+fighter(steve, selectable, [human], minecraft, 92, [ssb_ultimate], [computer], 2009).                           % not sure about this one with alex
+fighter(sephiroth, male, [human,jenova], final_fantasy, 79, [ssb_ultimate], [playstation], 1997).
+fighter(pyra, female, [blade,human], xenoblade, 98, [ssb_ultimate], [nintendo_switch], 2017).
+fighter(mythra, female, [blade,human], xenoblade, 98, [ssb_ultimate], [nintendo_switch], 2017).
+fighter(kazuya, male, [demon,human], tekken, 113, [ssb_ultimate], [arcade], 1994).
+fighter(sora, male, [human], kingdom_hearts, 85, [ssb_ultimate], [playstation_2], 2002).
---
+++ smashdle.pl
@@ -0,0 +1,117 @@
+:- include(fighters).
+:- dynamic guessCount/1.
+guessCount(0).
+
+play :- 
+        retractall(guessCount(_)),
+        assert(guessCount(0)),
+        selectFighter(Name),
+        write(Name),
+        guess(Name).
+
+selectFighter(Name) :- random_member(Name, [mario,donkey_kong,link,samus,dark_samus,yoshi,kirby,fox,
+pikachu,luigi,ness,captain_falcon,jigglypuff,peach,daisy,bowser,ice_climbers,sheik,dr_mario,pichu,
+falco,marth,lucina,young_link,ganondorf,mewtwo,roy,chrom,mr_game_and_watch,meta_knight,pit,dark_pit,
+zero_suit_samus,wario,snake,ike,pokemon_trainer,diddy_kong,lucas,sonic,king_dedede,olimar,lucario,
+rob,toon_link,wolf,villager,mega_man,wii_fit_trainer,rosalina_and_luna,little_mac,greninja,mii_brawler,
+mii_swordfighter,mii_gunner,palutena,pac_man,robin,shulk,bowser_jr,duck_hunt,ryu,ken,cloud,corrin,
+bayonetta,inkling,ridley,king_k_rool,isabelle,incineroar,piranha_plant,joker,hero,banjo_and_kazooie,
+terry,byleth,min_min,steve,sephiroth,pyra,mythra,kazuya,sora]).
+
+% selectFighter(Name) :- random_member(Name, [snake]).
+
+guess(Fighter) :- 
+        nl,
+        write('Make a Guess! '),
+        read(Guess),
+        compareGuess(Guess, Fighter).
+
+compareGuess(Guess, Answer) :-
+        \+ fighter(Guess,_ ,_ ,_ ,_ ,_ ,_ , _),
+        write('That is NOT a Fighter, Guess Again!'), nl,
+        guess(Answer).
+
+compareGuess(Guess, Answer) :-
+        \+ dif(Guess,Answer),
+        retract(guessCount(N)),
+        N1 is N + 1,
+        assert(guessCount(N1)),
+        write('Victory'), nl,
+        write('You Guessed: '),
+        write(Guess), nl,
+        write('Number of tries: '),
+        write(N1).
+
+compareGuess(Guess, Answer) :-
+        dif(Guess,Answer),
+        retract(guessCount(N)),
+        N1 is N + 1,
+        assert(guessCount(N1)),
+        fighter(Guess,Gender1,Species1,Universe1,Weight1,First1,Platform1,Date1),
+        fighter(Answer,Gender2,Species2,Universe2,Weight2,First2,Platform2,Date2),
+        write('Gender: '),
+        compareString(Gender1,Gender2),
+        write('Species: '),
+        compareList(Species1,Species2),
+        write('Universe: '),
+        compareString(Universe1,Universe2),
+        write('Weight: '),
+        compareNumber(Weight1,Weight2),
+        write('First Appearance: '),
+        compareList(First1,First2),
+        write('Platform of Origin: '),
+        compareList(Platform1,Platform2),
+        write('Origin Date: '),
+        compareNumber(Date1,Date2),
+        guess(Answer).
+        
+
+compareString(Guess, Answer) :-
+        \+ dif(Guess, Answer),
+        write('Green, '), 
+        write(Guess), nl.
+
+compareString(Guess, Answer) :-
+        dif(Guess,Answer),
+        write('Red, '),
+        write(Guess), nl.
+
+compareList(Guess, Answer) :-
+        \+ dif(Guess, Answer),
+        write('Green, '),
+        write(Guess), nl.
+
+compareList(Guess, Answer) :-
+        dif(Guess,Answer),
+        compareListRecursive(Guess,Answer),
+        write(Guess), nl.
+
+compareListRecursive([H1|_],Answer) :-
+        member(H1, Answer),
+        write('Orange, ').
+
+compareListRecursive([H1|T1],Answer) :-
+        \+ member(H1, Answer),
+        compareListRecursive(T1, Answer).
+
+compareListRecursive(Guess,_) :-
+        length(Guess,1),
+        write('Red, ').
+
+compareNumber(Guess, Answer) :-
+        \+ dif(Guess, Answer),
+        write('Green, '),
+        write(Guess),
+        write(Guess), nl.
+
+compareNumber(Guess, Answer) :-
+        dif(Guess, Answer),
+        Guess < Answer,
+        write('More, '),
+        write(Guess), nl.
+
+compareNumber(Guess, Answer) :-
+        dif(Guess, Answer),
+        Guess > Answer,
+        write('Less, '),
+        write(Guess), nl.
